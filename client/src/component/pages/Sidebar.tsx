be8854be6b2dd4c17e44cbf1b611a46eaea49545
saurabh <saurabh.warhade@auditflo.com>
2024-04-30 23:39:02 +0530

    add alltype product and admin and manager role restriction added at sidebar

--- client/src/component/pages/Sidebar.tsx
+++ client/src/component/pages/Sidebar.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useAuth } from '../Hook/useAuth'
 
 export default function Sidebar() {
+  const {isAdmin,isManager} = useAuth()
   return (
 <>
 <div className='min-w-[120px] h-full pt-10 fixed top-0 left-0  bg-gray-600  px-1'>
   <ul className='flex flex-col w-full'>
     <li className='w-full hover:bg-gray-700  py-2'>  <NavLink className={({isActive})=>{ return `text-white ${isActive ? " text-white drop-shadow-lg  shadow-white  font-bold  " :"text-gray-200 "}` }} to={"/product"}>Product</NavLink></li>
-    <li className='w-full hover:bg-gray-700  py-2'>  <NavLink to={"/create"} className={({isActive})=>{ return `text-white ${isActive ? "text-white drop-shadow-lg  shadow-white  font-bold " :" text-gray-200"}` }}>CreateProduct</NavLink></li>
-    <li className='w-full hover:bg-gray-700  py-2'>  <NavLink to={"/users"} className={({isActive})=>{ return `text-white ${isActive ? "text-white drop-shadow-lg  shadow-white  font-bold " :" text-gray-200"}` }}>Users</NavLink></li>
-    <li className='w-full hover:bg-gray-700  py-2'>  <NavLink to={"/category"} className={({isActive})=>{ return `text-white ${isActive ? "text-white drop-shadow-lg  shadow-white  font-bold " :" text-gray-200"}` }}>Category</NavLink></li>
+ { isAdmin &&  <li className='w-full hover:bg-gray-700  py-2'>  <NavLink to={"/create"} className={({isActive})=>{ return `text-white ${isActive ? "text-white drop-shadow-lg  shadow-white  font-bold " :" text-gray-200"}` }}>CreateProduct</NavLink></li>}
+ {isAdmin &&   <li className='w-full hover:bg-gray-700  py-2'>  <NavLink to={"/users"} className={({isActive})=>{ return `text-white ${isActive ? "text-white drop-shadow-lg  shadow-white  font-bold " :" text-gray-200"}` }}>Users</NavLink></li>}
+   {isAdmin && <li className='w-full hover:bg-gray-700  py-2'>  <NavLink to={"/category"} className={({isActive})=>{ return `text-white ${isActive ? "text-white drop-shadow-lg  shadow-white  font-bold " :" text-gray-200"}` }}>Category</NavLink></li>}
     <li className='w-full hover:bg-gray-700  py-2'>  <NavLink to={"/order/Create"} className={({isActive})=>{ return `text-white ${isActive ? "text-white drop-shadow-lg  shadow-white  font-bold " :" text-gray-200"}` }}>Create Order</NavLink></li>
     <li className='w-full hover:bg-gray-700  py-2'>  <NavLink to={"/order"} className={({isActive})=>{ return `text-white ${isActive ? "text-white drop-shadow-lg  shadow-white  font-bold " :" text-gray-200"}` }}>Orders</NavLink></li>
   </ul>
